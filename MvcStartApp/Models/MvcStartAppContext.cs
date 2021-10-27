e7de06ee29bbc5329dd7f7654e32121679c7484c
v-king777 <v.kolpashchikov@gmail.com>
2021-10-27 21:42:20 +0300

    27.10.21.21.42

---
+++ MvcStartApp/Models/MvcStartAppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcStartApp.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Models
+{
+    public class MvcStartAppContext : DbContext
+    {
+        public MvcStartAppContext(DbContextOptions<MvcStartAppContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
