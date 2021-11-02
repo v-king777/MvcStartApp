c2a61846da24b01c1f08267169c2bd1d85368cb4
v-king777 <v.kolpashchikov@gmail.com>
2021-11-02 11:56:44 +0300

    02.11.21.11.56

--- MvcStartApp/Controllers/UsersController.cs
+++ MvcStartApp/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MvcStartApp.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,22 @@ namespace MvcStartApp.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly IUserRepository _repo;
+
+        public UsersController(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _repo.GetUsers();
+
+            return View(users);
+        }
+
+
         [HttpGet]
         public IActionResult SignIn()
         {
